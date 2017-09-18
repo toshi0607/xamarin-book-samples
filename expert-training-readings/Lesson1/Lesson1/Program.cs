d8363ec7edb4ba9f936ba7f04fe45ed855e9888f
toshi0607 <s.toshi0607@gmail.com>
2017-09-18 21:16:18 +0900

    use Dictionary

--- expert-training-readings/Lesson1/Lesson1/Program.cs
+++ expert-training-readings/Lesson1/Lesson1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Lesson1
 {
@@ -6,10 +7,27 @@ namespace Lesson1
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("氏名を入力してください");
+            Dictionary<string, string> myDict = new Dictionary<string, string>();
 
-            string strName = Console.ReadLine();
-            Console.WriteLine($"こんにちは{strName}さん"); 
+            myDict.Add("0001", "高橋");
+            myDict.Add("0002", "佐々木");
+            myDict.Add("0003", "鈴木");
+            myDict.Add("0004", "前田");
+
+            foreach(string key in myDict.Keys)
+            {
+                Console.WriteLine(key);
+            }
+
+            foreach(string val in myDict.Values)
+            {
+                Console.WriteLine(val);
+            }
+
+            foreach(KeyValuePair<string, string> kvp in myDict)
+            {
+                Console.WriteLine($"Key={kvp.Key}, Value={kvp.Value}");
+            }
         }
     }
 }
