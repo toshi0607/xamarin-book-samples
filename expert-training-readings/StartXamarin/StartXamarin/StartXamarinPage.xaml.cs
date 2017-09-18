12a0a4cd3aa74b4cf34348e438a0c758693575c7
toshi0607 <s.toshi0607@gmail.com>
2017-09-18 18:21:11 +0900

    add skelton

---
+++ expert-training-readings/StartXamarin/StartXamarin/StartXamarinPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace StartXamarin
+{
+    public partial class StartXamarinPage : ContentPage
+    {
+        public StartXamarinPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
