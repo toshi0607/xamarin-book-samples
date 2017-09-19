532bc157fecfc1b3aaaaea7e0cdcac1b292151f1
toshi0607 <s.toshi0607@gmail.com>
2017-09-20 00:43:50 +0900

    add event handlers fot buttons

--- expert-training-readings/SampleApp/SampleApp/SampleAppPage.xaml.cs
+++ expert-training-readings/SampleApp/SampleApp/SampleAppPage.xaml.cs
@@ -17,6 +17,31 @@ namespace SampleApp
             {
                 DisplayAlert("警告", "タブ1のボタン2がタップされました", "OK");
             };
+
+            Authentication.Clicked += (sender, e) =>
+            {
+                if (!string.IsNullOrEmpty(entryAccount.Text))
+                {
+                    if (!string.IsNullOrEmpty(entryPassword.Text))
+                    {
+                        DisplayAlert("通知", $"{entryAccount.Text}を認証しました", "OK");
+                    }
+                    else
+                    {
+                        DisplayAlert("警告", "パスワードを入力してください", "OK");
+                    }
+                }
+                else
+                {
+                    DisplayAlert("警告", "アカウントを入力してください", "OK");
+                }
+            };
+
+            Clear.Clicked += (sender, e) =>
+            {
+                entryAccount.Text = "";
+                entryPassword.Text = "";
+            };
         }
     }
 }
