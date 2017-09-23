b7e37d99a61882c659e421e558db6192acc2a8c9
toshi0607 <s.toshi0607@gmail.com>
2017-09-23 13:32:55 +0900

    add http client

---
+++ expert-training-readings/AdressSearchApp/AdressSearchApp/HttpService.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AdressSearchApp
+{
+    public class HttpService
+    {
+        public static async Task<dynamic> getDataFromService(string queryString)
+        {
+            dynamic data = null;
+            HttpClient client = new HttpClient();
+            var response = await client.GetAsync(queryString);
+
+            if ((response != null) && !((int)response.StatusCode >= 400))
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                data = JsonConvert.DeserializeObject(json);
+            }
+            return data;
+        }
+    }
+}
