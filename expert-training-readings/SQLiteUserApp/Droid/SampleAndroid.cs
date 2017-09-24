58e52716979437680350aac5152d754e51072bb7
toshi0607 <s.toshi0607@gmail.com>
2017-09-24 10:42:02 +0900

    android dependency

---
+++ expert-training-readings/SQLiteUserApp/Droid/SampleAndroid.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+using SQLiteUserApp.Droid;
+using Xamarin.Forms;
+using SQLiteSample;
+
+[assembly: Dependency(typeof(SampleAndroid))]
+
+namespace SQLiteUserApp.Droid
+{
+    public class SampleAndroid : SQLiteInterface
+    {
+        public SQLite.Net.SQLiteConnection GetConnection()
+        {
+            var wSqlitePath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            const string wSqliteName = "SampleSQLite.db";
+            var wPath = Path.Combine(wSqlitePath, wSqliteName);
+            var wPlatform = new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid();
+            var wConnect = new SQLite.Net.SQLiteConnection(wPlatform, wPath);
+            return wConnect;
+        }
+    }
+}
