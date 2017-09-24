94edd1046518c307f608e2e231e4b0778a6df281
toshi0607 <s.toshi0607@gmail.com>
2017-09-24 20:25:27 +0900

    add view controller

---
+++ native/KitchenTimeriOS/KitchenTimeriOS/MyViewController.cs
@@ -0,0 +1,13 @@
+using Foundation;
+using System;
+using UIKit;
+
+namespace KitchenTimeriOS
+{
+    public partial class MyViewController : UIViewController
+    {
+        public MyViewController (IntPtr handle) : base (handle)
+        {
+        }
+    }
+}
