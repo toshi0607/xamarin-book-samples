642f88d143e1ff79ad8b533d3420d57d901adc1c
toshi0607 <s.toshi0607@gmail.com>
2017-09-27 00:25:20 +0900

    add model

---
+++ native/XMemo/XMemo/Memo.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace XMemo
+{
+    public class Memo
+    {
+        public DateTime Date { get; set; }
+        public string Subject { get; set; }
+        public string Text { get; set; }
+    }
+}
