c40306f4c03100075fc0a2d5f2937f80d9f2121c
toshi0607 <s.toshi0607@gmail.com>
2017-09-27 00:57:24 +0900

    add framewprk

---
+++ native/XMemo/iOS/ViewController.designer.cs
@@ -0,0 +1,67 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+
+namespace XMemo.iOS
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        UIKit.UIButton Button { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField DateText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton LoadButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextView MemoText { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton SaveButton { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UITextField SubjectText { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (DateText != null) {
+                DateText.Dispose ();
+                DateText = null;
+            }
+
+            if (LoadButton != null) {
+                LoadButton.Dispose ();
+                LoadButton = null;
+            }
+
+            if (MemoText != null) {
+                MemoText.Dispose ();
+                MemoText = null;
+            }
+
+            if (SaveButton != null) {
+                SaveButton.Dispose ();
+                SaveButton = null;
+            }
+
+            if (SubjectText != null) {
+                SubjectText.Dispose ();
+                SubjectText = null;
+            }
+        }
+    }
+}
