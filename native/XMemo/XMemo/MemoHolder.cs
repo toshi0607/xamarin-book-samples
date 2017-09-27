6fb47f303238dda5d1fbbb4a205c756f58b70a9e
toshi0607 <s.toshi0607@gmail.com>
2017-09-27 22:46:43 +0900

    add MemoHilder class

---
+++ native/XMemo/XMemo/MemoHolder.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace XMemo
+{
+    public class MemoHolder
+    {
+        public static MemoHolder Current { get; } = new MemoHolder();
+        public Memo Memo { get; set; }
+    }
+}
