b6433533dd096253b359af416260b70ebd46bfd3
toshi0607 <s.toshi0607@gmail.com>
2017-09-30 10:51:46 +0900

    add todoholder model

---
+++ native/MvxApp/MvxApp/Models/TodoHolder.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+namespace MvxApp.Models
+{
+    public class TodoHolder
+    {
+        public static TodoHolder Current { get; } = new TodoHolder();
+        public List<Todo> TodoList { get; set; } = new List<Todo>();
+    }
+}
