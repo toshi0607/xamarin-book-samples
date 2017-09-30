41c02b978ec0e5bcba0915554647b4b8344e7ecf
toshi0607 <s.toshi0607@gmail.com>
2017-09-30 11:02:48 +0900

    add register viewmodel

---
+++ native/MvxApp/MvxApp/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using MvvmCross.Core.ViewModels;
+
+namespace MvxApp.ViewModels
+{
+    public class RegisterViewModel : MvxViewModel
+    {
+        public DateTime Date { get; set; }
+        public string Text { get; set; }
+
+        public RegisterViewModel()
+        {
+            Date = DateTime.Now;
+            Text = "";
+        }
+    }
+}
